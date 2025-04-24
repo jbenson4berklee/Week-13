4f2c43e583bd6427b915d7140ec9e78f64d6b521
James Benson <jbenson4@berklee.edu>
2025-04-24 08:42:29 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,6 @@
+mix_of_stuff = [3, 3.6, "trees", True]
+
+playlist_for_coding = ["The Rocky Road To Dublin", "Danny Don't You Know", "The Bidding", "Sugar Pill", "Mad IQ"]
+print (playlist_for_coding[1])
+
+playlist_for_coding.append ("Never Going To Give You Up by Rick Astley")
